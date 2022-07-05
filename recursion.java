bebb031157da0475ff1efcb7f77a088b5c649161
marawan emad <88719313+Marawanemad@users.noreply.github.com>
2022-07-05 18:38:09 +0200

    Update recursion.java

--- recursion.java
+++ recursion.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class JavaApplication3 {   
+public class recursion {   
     public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     System.out.print("Enter the first number : ");
